01829bf37acbca79abe692bd11af55f8dd610eab
zhongnan <3011168478@qq.com>
2021-02-08 23:46:19 +0800

    首次提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import service from './util/service'
 
+Vue.use(Antd);
 Vue.config.productionTip = false
+Vue.prototype.$axios = service
 
 new Vue({
   router,
---
+++ src/util/service.js
@@ -0,0 +1,36 @@
+import Vue from "vue"
+import axios from "axios";
+import {message} from 'ant-design-vue';
+
+
+console.info(message)
+const service = axios.create({
+  timeout: 5000
+});
+// 添加请求拦截器
+service.interceptors.request.use(config => {
+  // 在发送请求之前做些什么
+  config.headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    // "Token":"1AB4B14DE0AF1ACBEE8CA44C4CED50C18DC7DCDFCF3AF65120339B00BBFBA2E9F"
+  }
+  config.data.Token ="1AB4B14DE0AF1ACBEE8CA44C4CED50C18DC7DCDFCF3AF65120339B00BBFBA2E9F"
+  return config;
+}, error => {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+
+// 添加响应拦截器
+service.interceptors.response.use(( { data } )=>{
+  // 对响应数据做点什么
+  if(data.code != "0")
+    Message.error(data.msg)
+  return data;
+},
+error =>{
+  return Promise.reject(error);
+});
+
+export default service
